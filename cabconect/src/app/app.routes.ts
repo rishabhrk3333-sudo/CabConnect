740cbfec79398e864d09296f46a61bd24e66f5aa
rishabhrk3333-sudo <rishabhrk3333@gmail.com>
2026-09-07 16:31:32 +0530

    created login and header component

--- cabconect/src/app/app.routes.ts
+++ cabconect/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('../app/features/auth/login/login').then(m => m.Login)
+    }
+];
